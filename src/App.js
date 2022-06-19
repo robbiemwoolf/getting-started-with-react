647576bc96ab3a9222edfc4fd3b7d3b244a4a7c4
Robbie Woolf <robbiemwoolf@gmail.com>
2022-06-19 13:31:28 -0700

    practice using js expressions in jsx component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const name = "Robbie";
+  const url = "https://github.com/robbiemwoolf";
+  const linkText = "GitHub profile";
+  const element = (
+    <p>
+      {" "}
+      Hey, Robbie! Go checkout your<a href={url}>{linkText}</a>.
+    </p>
+  );
+
+  return element;
 };
 
 export default App;
